a4165a353222775db609512a320cd832f486de52
alexcuba96@gmail.com <alexcuba96@gmail.com>
2022-04-29 12:24:13 -0500

    Config

---
+++ config-service/src/test/java/com/examen/mercadolibre/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.mercadolibre.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
