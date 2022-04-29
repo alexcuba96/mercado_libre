53ddd11860b2cfa6daad5d8ac511b7f3fdfcba3a
alexcuba96@gmail.com <alexcuba96@gmail.com>
2022-04-29 16:00:06 -0500

    Enable Eureka Client

--- examen/src/main/java/com/examen/mercadolibre/examen/ExamenApplication.java
+++ examen/src/main/java/com/examen/mercadolibre/examen/ExamenApplication.java
@@ -2,8 +2,10 @@ package com.examen.mercadolibre.examen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ExamenApplication {
 
 	public static void main(String[] args) {
